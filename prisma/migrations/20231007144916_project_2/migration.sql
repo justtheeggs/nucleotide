00d128d1419266dea1b650a4e263f5c5720bbf59
ixatomic <sauvejared@gmail.com>
2023-10-07 12:24:31 -0400

    Tables for project

--- prisma/migrations/20231007144916_project_2/migration.sql
+++ prisma/migrations/20231007144916_project_2/migration.sql
@@ -1,3 +1,6 @@
+-- CreateEnum
+CREATE TYPE "ProjectRoles" AS ENUM ('Owner', 'Moderator', 'Member');
+
 -- CreateTable
 CREATE TABLE "Account" (
     "id" TEXT NOT NULL,
@@ -44,6 +47,47 @@ CREATE TABLE "VerificationToken" (
     "expires" TIMESTAMP(3) NOT NULL
 );
 
+-- CreateTable
+CREATE TABLE "Tag" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "fabricated" BOOLEAN NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "approved_at" TIMESTAMP(3) NOT NULL,
+    "deleted" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Tag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3) NOT NULL,
+    "deleted" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProjectRequests" (
+    "id" TEXT NOT NULL,
+    "project_id" TEXT NOT NULL,
+
+    CONSTRAINT "ProjectRequests_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProjectMember" (
+    "id" TEXT NOT NULL,
+    "project_id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "role" "ProjectRoles" NOT NULL DEFAULT 'Owner',
+
+    CONSTRAINT "ProjectMember_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
 
@@ -64,3 +108,12 @@ ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId"
 
 -- AddForeignKey
 ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectRequests" ADD CONSTRAINT "ProjectRequests_project_id_fkey" FOREIGN KEY ("project_id") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectMember" ADD CONSTRAINT "ProjectMember_project_id_fkey" FOREIGN KEY ("project_id") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectMember" ADD CONSTRAINT "ProjectMember_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
