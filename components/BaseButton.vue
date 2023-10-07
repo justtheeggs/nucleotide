61204f124d43682c2721448fa51aea7f8b642bc9
YakHolloway <43993682+YakHolloway@users.noreply.github.com>
2023-10-07 16:01:19 -0400

    Style Guide

---
+++ components/BaseButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <NuxtLink
+    class="px-8 py-4 font-inter text-center text-white cursor-pointer rounded-lg inline-block font-bold text-lg my-8 lg:my-0"
+    :to="disabled?undefined:props.to"
+  >
+    <slot />
+  </NuxtLink>
+</template>
+<script lang="ts" setup>
+import { RouteLocationRaw } from '#vue-router'
+
+const props = defineProps({
+  to: { type: Object as PropType<RouteLocationRaw> },
+  disabled: { type: Boolean }
+})
+
+function block () {
+  alert('here')
+  return false
+}
+</script>
---
+++ pages/style-guide.vue
@@ -0,0 +1,36 @@
+<template>
+  <DefaultLayout title="Style Guide">
+    <SubHeader>This is a sub header(H2)</SubHeader>
+    <p>
+      This is a paragraph of normal text. Bacon ipsum dolor amet shank salami tail rump capicola,
+      turducken boudin kevin landjaeger doner pork tri-tip brisket hamburger. Tri-tip boudin brisket
+      jerky rump shankle salami tail tenderloin short ribs sirloin shank swine venison. Beef jowl
+      tenderloin filet mignon cow doner. Biltong flank turkey tail rump doner alcatra beef ribs
+      chuck shoulder pastrami prosciutto. Boudin capicola pig tenderloin andouille shoulder salami
+      shank. Ribeye bresaola cupim shankle. Brisket strip steak chuck ham hock pig pork belly.
+    </p>
+    <div>
+      <SubHeader>Buttons</SubHeader>
+      <PrimaryButton class="mr-4">
+        Primary Button
+      </PrimaryButton>
+      <SubmitButton class="mx-4" @click="sendAlert">
+        Submit Button
+      </SubmitButton>
+      <CancelButton class="mx-4">
+        Cancel Button
+      </CancelButton>
+      <PrimaryButton to="/about" disabled class="mx-4">
+        Disabled Primary Button
+      </PrimaryButton>
+      <SubmitButton disabled class="ml-4">
+        Disabled Submit Button
+      </SubmitButton>
+    </div>
+  </DefaultLayout>
+</template>
+<script setup lang="ts">
+function sendAlert () {
+  alert('here')
+}
+</script>
