f98f7715e57801af9198f6ad052c0d41c70d3754
YakHolloway <43993682+YakHolloway@users.noreply.github.com>
2023-10-07 14:13:09 -0400

    Heading Components and About Styling

---
+++ components/SubHeader.vue
@@ -0,0 +1,5 @@
+<template>
+  <h2 class="text-2xl font-bold my-4 text-cyan-950">
+    <slot />
+  </h2>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,6 @@
 <template>
-  <DefaultLayout>
+  <DefaultLayout title="About">
     <div class="main-container">
-      <h1>About</h1>
       <h2>What is Nucleotide?</h2>
       <p>
         Interested in science?  Have a science project that needs help?  Nucleotide can help.  Nucleotide is a tool for connecting projects with team members.
@@ -11,14 +10,14 @@
       <p>
         The tool also provides recomendations to users about available projects they might be interested in.
       </p>
-      <h2>
+      <SubHeader>
         Getting Started
-      </h2>
+      </SubHeader>
       <p>
         To get started create a user, view projects, and find something that interests you.  Be sure to add your skills
         so that project owners can find you and match you to their projects.
       </p>
-      <h2>Who is Just the Eggs</h2>
+      <SubHeader>Who is Just the Eggs</SubHeader>
       <p>
         Just the Eggs is spin off group from Just the Ducks.  We are a group of professional coders who enjoy a good challenge.
         We are a group of 6 coders by day and NASA enthusiasts by night. Our group was formed while seeking unique coding
