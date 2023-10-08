39094fb91a9c02db2f9011d73b6a9415921208ee
Brendan Fuller <git@brendanfuller.com>
2023-10-08 13:16:07 -0400

    Update project

--- pages/projects/[id]/update.vue
+++ pages/projects/[id]/update.vue
@@ -1,29 +1,105 @@
 <template>
-  <DefaultLayout title="Discover">
-    <template v-if="projects.data != null">
-      <div v-for="project in cProjects">
-        <ProjectItem :project="project" :key="project.id" />
-      </div>
-    </template>
+  <DefaultLayout title="Update/Edit Project">
+    <NCard class="bg-slate-100">
+      <b>Name</b>
+      <br />
+      <NInput
+        type="text"
+        class="border outline-none mt-2"
+        v-model:value="name"
+      />
+    </NCard>
+    <br />
+    <NCard class="bg-slate-100">
+      <b>Description</b>
+      <br />
+      <NInput
+        type="textarea"
+        class="border outline-none mt-2"
+        v-model:value="description"
+      />
+    </NCard>
+
+    <NCard class="bg-slate-100">
+      <b>Status</b>
+      <br />
+      <select v-model="status">
+        <option :value="ProjectStatus.Planning_Design">Planning/Design</option>
+        <option :value="ProjectStatus.Pending">Pending</option>
+        <option :value="ProjectStatus.Active">Active</option>
+        <option :value="ProjectStatus.Completed">Completed</option>
+      </select>
+    </NCard>
+
+    <p class="text-green-400">{{ message }}</p>
+
+    <NButton @click="updateProject" class="mt-2">Update Project</NButton>
   </DefaultLayout>
 </template>
 
 <script setup lang="ts">
-import { NInput } from "naive-ui";
+import { NInput, NButton, NSelect, NCard } from "naive-ui";
 import { DefaultLayout } from "#components";
-import { Project } from "@prisma/client";
+import { Project, ProjectStatus } from "@prisma/client";
 
 type ProjectWithTags<T> = Partial<T> & { tags: string[] };
 
 const { $client } = useNuxtApp();
 
-const projects = await $client.discover.getAllProjects.useQuery();
+const route = useRoute();
 
-const cProjects = computed(() =>
-  projects.data?.value.map((project) => {
-    return project;
-  })
-);
+const status = ref<string>("");
 
-const test = ref("");
+const checkStatus = (status: ProjectStatus) => {
+  console.log(project.data?.value?.status);
+  console.log(project.data?.value?.status == status);
+  if (status == project.data?.value?.status) {
+    return true;
+  }
+  return false;
+};
+
+const statusOptions = [
+  { label: "Planning/Design", value: ProjectStatus.Planning_Design },
+  { label: "Active", value: "Planning_Active" },
+  { label: "Pending", value: ProjectStatus.Pending },
+  { label: "Completed", value: ProjectStatus.Completed },
+];
+
+const project = await $client.projects.get_individual_project.useQuery({
+  id: route.params.id as string,
+});
+
+onMounted(() => {
+  if (project.data != undefined) {
+    console.log(project);
+    name.value = project.data.value?.name as string;
+    description.value = project.data.value?.description as string;
+    status.value = project.data.value?.status as string;
+  }
+});
+
+const name = ref<string | null>(null);
+const description = ref<string | null>(null);
+
+const message = ref<string | null>(null);
+const updateProject = async () => {
+  if (name.value != null || description.value != null) {
+    const result = await $client.projects.update_project.mutate({
+      id: route.params.id as string,
+      name: name.value ?? "Untitled Project",
+      description: description.value ?? "No description provided",
+      status: status.value,
+    });
+
+    if ((result.status = true)) {
+      message.value = result.message;
+    } else {
+      message.value = result.message;
+      alert("Could not update project");
+    }
+  } else {
+    console.log("example");
+  }
+};
 </script>
--- server/trpc/routers/projects.ts
+++ server/trpc/routers/projects.ts
@@ -1,136 +1,167 @@
 import { object, z } from "zod";
 import { privateProcedure, publicProcedure, router } from "../trpc";
+import { ProjectStatus } from "@prisma/client";
 
 const id = z.object({
-    id: z.string(), 
-})
+  id: z.string(),
+});
 
 const img = z.object({
-    id: z.string(),
-    base64: z.string(), 
-})
+  id: z.string(),
+  base64: z.string(),
+});
 
 const ids = z.object({
-    ids: z.array(z.string())
-})
+  ids: z.array(z.string()),
+});
 
 /**
  * This is an example of a public procedure.
  * It requires a `text` input and returns a greeting.
  */
 export const projectRouter = router({
-    hello: publicProcedure
-        .input(
-        z.object({
-            text: z.string().nullish(),
-        })
-        )
-        .query(({ input, ctx }) => {
-        return {
-            greeting: `hello from router ${input?.text ?? "world"}`,
-            time: new Date(),
-        };
-        }),
+  hello: publicProcedure
+    .input(
+      z.object({
+        text: z.string().nullish(),
+      })
+    )
+    .query(({ input, ctx }) => {
+      return {
+        greeting: `hello from router ${input?.text ?? "world"}`,
+        time: new Date(),
+      };
+    }),
 
-    get_project_tagnames: publicProcedure
-        .input(ids)
-        .query( async ({input, ctx}) => {
-            const text = await ctx.prisma.tag.findMany({
-                where: {
-                    id: { in: input.ids }
-                }
-            })
-            console.log("hello world")
-            console.log(text)
-            return text;
-        }),
+  get_project_tagnames: publicProcedure
+    .input(ids)
+    .query(async ({ input, ctx }) => {
+      const text = await ctx.prisma.tag.findMany({
+        where: {
+          id: { in: input.ids },
+        },
+      });
+      console.log("hello world");
+      console.log(text);
+      return text;
+    }),
 
-    get_project_members: publicProcedure
-        .input(ids)
-        .query(async ({input, ctx}) => {
-            return await ctx.prisma.user.findMany({
-                where: {
-                    id: { in: input.ids }
-                }
-            })
-        }),
-    
-    get_project_links: publicProcedure
-        .input(ids)
-        .query(async ({input, ctx}) => {
-            return await ctx.prisma.user.findMany({
-                where: {
-                    id: { in: input.ids }
-                }
-            })
-        }),
+  get_project_members: publicProcedure
+    .input(ids)
+    .query(async ({ input, ctx }) => {
+      return await ctx.prisma.user.findMany({
+        where: {
+          id: { in: input.ids },
+        },
+      });
+    }),
 
-    get_project_users: publicProcedure
-        .input(ids)
-        .query(async ({input, ctx}) => {
-            return await ctx.prisma.category.findMany({
-                where: {
-                    id: {in: input.ids}
-                }
-            })
-        }),
+  get_project_links: publicProcedure
+    .input(ids)
+    .query(async ({ input, ctx }) => {
+      return await ctx.prisma.user.findMany({
+        where: {
+          id: { in: input.ids },
+        },
+      });
+    }),
 
-    get_individual_project: publicProcedure.input(id).query(async ({ input, ctx }) => {
-        const projects = await ctx.prisma.project.findUnique({
-            where: {
-                id: input.id
-            },
+  get_project_users: publicProcedure
+    .input(ids)
+    .query(async ({ input, ctx }) => {
+      return await ctx.prisma.category.findMany({
+        where: {
+          id: { in: input.ids },
+        },
+      });
+    }),
+
+  get_individual_project: publicProcedure
+    .input(id)
+    .query(async ({ input, ctx }) => {
+      const projects = await ctx.prisma.project.findUnique({
+        where: {
+          id: input.id,
+        },
+        include: {
+          links: true,
+          tags: {
             include: {
-            links: true,
-            tags: {
-                include: {
-                tag: true,
-                },
+              tag: true,
             },
-            members: {
-                include: {
-                    user: true,
-                },
+          },
+          members: {
+            include: {
+              user: true,
             },
+          },
+        },
+      });
+
+      return projects;
+    }),
+  update_project: privateProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        description: z.string(),
+        status: z.string(),
+        id: z.string(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      console.log(input);
+      const result = await ctx.prisma.project.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          name: input.name,
+          description: input.description,
+          status: input.status as ProjectStatus,
+        },
+        include: {
+          members: {
+            where: {
+              user_id: ctx.user?.user?.id,
             },
-        });
-    
-        return projects;
-        }),
+          },
+        },
+      });
+      return {
+        message: "Project updated",
+        status: true,
+      };
+    }),
+  // get_individual_project: publicProcedure
+  // .input(id)
+  // .query( async ({ input, ctx }) => {
+  //     return await ctx.prisma.project.findFirst({
+  //         where: {
+  //             id: input.id,
+  //         },
+  //         include: {
+  //             tags: true,
+  //             members: true,
+  //             links: true,
+  //             categories: true,
+  //         }
+  //     })
+  // }),
 
-        // get_individual_project: publicProcedure
-        // .input(id)
-        // .query( async ({ input, ctx }) => {
-        //     return await ctx.prisma.project.findFirst({
-        //         where: {
-        //             id: input.id,
-        //         },
-        //         include: {
-        //             tags: true,
-        //             members: true,
-        //             links: true,
-        //             categories: true,
-        //         }
-        //     })
-        // }),
+  // submit_image: privateProcedure
+  //     .input(img)
+  //     .query(({input, ctx}) => {
+  //         const result = ctx.prisma.project.update({
+  //             where: {
+  //                 id: input.id,
+  //             },
+  //             data: {
+  //                 project: {
 
-    
-    // submit_image: privateProcedure
-    //     .input(img)
-    //     .query(({input, ctx}) => {
-    //         const result = ctx.prisma.project.update({
-    //             where: {
-    //                 id: input.id,
-    //             },
-    //             data: {
-    //                 project: {
-
-    //                 }
-    //             }
-    //         })
-    //     })
-    // }),
-
-    
+  //                 }
+  //             }
+  //         })
+  //     })
+  // }),
 });
-
