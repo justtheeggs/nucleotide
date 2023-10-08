c8bb9c84489c6ad3d954850f93999524b2f1dfe5
Brendan Fuller <git@brendanfuller.com>
2023-10-08 09:12:51 -0400

    Add about in bio

--- components/profile/ProfileAbout.vue
+++ components/profile/ProfileAbout.vue
@@ -3,8 +3,15 @@ import { NText } from 'naive-ui';
   <NCard class="bg-slate-100">
     <b>About</b>
     <br />
-    <NInput class="mt-2" type="textarea" v-model:value="about" />
-
+    <NInput
+      class="mt-2"
+      type="textarea"
+      v-model:value="about"
+      @keydown="keyDown"
+    />
+    <p class="text-green-400">
+      {{ message }}
+    </p>
     <NButton @click="updateAbout" class="mt-2">Update Bio</NButton>
   </NCard>
 </template>
@@ -14,5 +21,22 @@ import { NInput, NButton, NCard } from "naive-ui";
 
 const about = ref("");
 
-const updateAbout = () => {};
+const { $client } = useNuxtApp();
+
+const message = ref<string | null>(null);
+
+const getAbout = await $client.user.getAbout.useQuery();
+
+onMounted(() => {
+  about.value = getAbout.data?.value as string;
+});
+
+const keyDown = () => {
+  message.value = null;
+};
+
+const updateAbout = () => {
+  $client.user.updateAbout.mutate({ about: about.value });
+  message.value = "Updated!";
+};
 </script>
